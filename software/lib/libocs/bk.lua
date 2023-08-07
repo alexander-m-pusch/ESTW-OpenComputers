88826ef59b1153b35cf3b3f7850aedc7f484a931
Alexander Pusch <alexander.m.pusch@t-online.de>
2023-08-07 18:23:40 +0200

    Create bk.lua

---
+++ software/lib/libocs/bk.lua
@@ -0,0 +1,36 @@
+local bkack = {}
+
+function bkack:init(ocs)
+  local o = {
+    ["bus"] = "block",
+    ["ocs"] = ocs,
+  }
+
+  setmetatable(o, self)
+  self.__index = self
+
+  return o
+end
+
+function bkack:send(nummer, befehl)
+  local inhalt = {
+    ["typ"] = self.bus,
+    ["inhalt"] = {
+      ["nummer"] = nummer,
+      ["befehl"] = befehl,
+    },
+  }
+
+  self["ocs"]:send(inhalt)
+end
+
+function bkack:callback(inhalt)
+  if not self["callback_internal"] then return false end
+  return self["callback_internal"](inhalt["nummer"], inhalt["befehl"])
+end
+
+function bkack:subscribe(fun)
+  self["callback_internal"] = fun
+end
+
+return bkack
